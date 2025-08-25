d2e3065ac9ebd177ad3147181629e4d72d01f92f
amiravni16 <amiravni13@gmail.com>
2025-08-25 21:13:45 +0300

    Initialize repository with CLI generated files and toys model

---
+++ src/types/toy.js
@@ -0,0 +1,12 @@
+export const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle',
+'Outdoor', 'Battery Powered']
+
+export const toy = {
+    _id: 't101',
+    name: 'Talking Doll',
+    imgUrl: 'hardcoded-url-for-now',
+    price: 123,
+    labels: ['Doll', 'Battery Powered', 'Baby'],
+    createdAt: 1631031801011,
+    inStock: true,
+}
