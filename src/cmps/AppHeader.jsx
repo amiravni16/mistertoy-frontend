3ce29fcd044bf6bf728c13330989f09da0f42b3e
amiravni16 <amiravni13@gmail.com>
2025-09-01 20:43:10 +0300

    Refactor AppHeader component to improve layout; add login link for user authentication and adjust container structure for better responsiveness.

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -2,16 +2,19 @@ import { NavLink } from 'react-router-dom'
 
 export function AppHeader() {
     return (
-        <section className="app-header container">
-            <div className="flex justify-between">
-                <nav>
-                    <NavLink to="/">home</NavLink> |
-                    <NavLink to="/toy">toys</NavLink> |
-                    <NavLink to="/dashboard">dashboard</NavLink> |
-                    <NavLink to="/about">about</NavLink>
-                </nav>
+        <section className="app-header">
+            <div className="container">
+                <div className="flex justify-between">
+                    <nav>
+                        <NavLink to="/">home</NavLink> |
+                        <NavLink to="/toy">toys</NavLink> |
+                        <NavLink to="/dashboard">dashboard</NavLink> |
+                        <NavLink to="/about">about</NavLink> |
+                        <NavLink to="/login">login</NavLink>
+                    </nav>
+                </div>
+                <div className="logo">Mister Toy</div>
             </div>
-            <div className="logo">Mister Toy</div>
         </section>
     )
 }
