1d0012a1872632fecde7e92348bc520020f510db
amiravni16 <amiravni13@gmail.com>
2025-09-17 15:36:15 +0300

    Update BASE_URL in http.service.js to support environment variable for API URL

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-const BASE_URL = 'http://localhost:3030/api/'
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3030/api/'
     
 const axios = Axios.create({
     withCredentials: true,
